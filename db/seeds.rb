a64767d830b3686f0f8b7b1ffff19ae3eb01de81
Jay Hayes <ur@iamvery.com>
2016-03-03 11:33:34 -0500

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+author = Author.create!(name: "Jay")
+Article.create!(title: "First post!", author: author).tap do |article|
+  article.comments.create!(body: "Nice one!")
+  article.comments.create!(body: "Good read")
+end
+Article.create!(title: "How to even", author: author).tap do |article|
+  article.comments.create!(body: "Not interesting")
+  article.comments.create!(body: "I literally can't")
+end
